1ae03341f03585121252bbdc0b9ff148874b2c5c
areebakhan3 <117973912+areebakhan3@users.noreply.github.com>
2024-05-16 11:46:43 -0700

    Add files via upload

---
+++ E-Commerce (Perfume)Website/connection.php
@@ -0,0 +1,20 @@
+<!-- config.php -->
+
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "e-commerce";
+
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
